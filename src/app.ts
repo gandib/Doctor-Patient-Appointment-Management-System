27a711dc4259807e6b063e9eb478b7360393611d
gandib <55914763+gandib@users.noreply.github.com>
2025-06-28 13:12:10 +0600

    Project initialization completed

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  }),
+);
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// app.use('/', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Doctor-Patient Appointment Management System!');
+});
+
+// Error handlers
+// app.use(globalErrorHandler);
+// app.use(notFound);
+
+export default app;
